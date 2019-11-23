77a0f66f8f1d912fbfd1f0fb0791e53b9494c295
Gabriela Zambrano <gzamb@users.noreply.github.com>
2019-11-23 11:09:58 -0700

    More changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from subprocess import call
 
 import picamera
 from picam3 import motion
@@ -20,6 +21,19 @@ def capture_image(current_time, pic_path):
         camera.resolution = (1280, 720)
         camera.capture(f"{pic_path}/{pic_name}")
     print("Picture taken")
+    return pic_name
+
+
+def stamp_picture(current_time, pic_path, pic_name):
+    # Get full path
+    full_path = f"{pic_path}/{pic_name}"
+    # Create message to stamp on picture
+    message = current_time.strftime("%Y.%m.%d - %h:%M:%S")
+    # Create the command
+    command = f"/usr/bin/convert {full_path} -pointsize 36 -fill red -annotate +700+650 '{message}' {full_path}"
+    # Execute the command
+    call([command], shell=True)
+    print("Picture has been timestamped")
 
 
 while True:
@@ -31,4 +45,6 @@ while True:
     motion_state = motion()
     print(motion_state)
     if motion_state:
-        capture_image(get_time(), pic_path)
+        current_time = get_time()
+        pic_name = capture_image(current_time, pic_path)
+        stamp_picture(current_time, pic_path, pic_name)
