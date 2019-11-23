158bff87b4f25d50d329e4cea8c2d9e497db5a67
Gabriela Zambrano <gzamb@users.noreply.github.com>
2019-11-23 10:35:18 -0700

    Adding take picture command in new main

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from picam3 import motion
+import picamera
 
 motion_state = False
 
 while True:
     motion_state = motion()
     print(motion_state)
+    if motion_state:
+        with picamera.PiCamera() as camera:
+            camera.resolution = (1280, 720)
+            camera.capture("motion_pic.jpg")
+        print("Picture taken")
